1442ef7f47d74990b2ec423c017d68762bf1e493
Diki Andriansyah <diki1aap@gmail.com>
2016-12-08 07:42:29 +0700

    Clearing config

--- bin/themes/update-arc-theme.sh
+++ bin/themes/update-arc-theme.sh
@@ -1,28 +1,20 @@
 #!/bin/bash
 
-# Changes gtk theme to Adwaita
-gsettings set org.gnome.desktop.interface gtk-theme "Adwaita"
-
-# Move to Temporary directory, for safe.
+# Move to Temporary directory.
 cd ~/Temporary
 
 # Remove old arc theme.
 sudo rm -rf /usr/share/themes/{Arc,Arc-Darker,Arc-Dark}
-rm -rf ~/.local/share/themes/{Arc,Arc-Darker,Arc-Dark}
-rm -rf ~/.themes/{Arc,Arc-Darker,Arc-Dark}
 
 # Cloning from GitHub.
 git clone https://github.com/horst3180/arc-theme --depth 1 && cd arc-theme
 
 # Configuration for elementary OS.
-./autogen.sh --prefix=/usr --disable-darker --disable-dark --disable-cinnamon --disable-gnome-shell --disable-unity --disable-xfwm
+./autogen.sh --prefix=/usr --disable-cinnamon --disable-gnome-shell --disable-unity --disable-xfwm
 
 # Installation.
 sudo make install
 
-# Changes gtk theme to Arc.
-gsettings set org.gnome.desktop.interface gtk-theme "Arc"
-
 # Cleaning.
-cd ..
+cd ~/Temporary
 rm -rf arc-theme
