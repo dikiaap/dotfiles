4be11c11a681d7b7b05d772777bf2dd483ce8e8b
Diki Andriansyah <diki1aap@gmail.com>
2016-12-20 18:30:04 +0700

    Update comment in update-arc-theme

--- bin/themes/update-arc-theme.sh
+++ bin/themes/update-arc-theme.sh
@@ -9,7 +9,7 @@ sudo rm -rf /usr/share/themes/{Arc,Arc-Darker,Arc-Dark}
 # Cloning from GitHub.
 git clone https://github.com/horst3180/arc-theme --depth 1 && cd arc-theme
 
-# Configuration for elementary OS.
+# Configuration.
 ./autogen.sh --prefix=/usr --disable-cinnamon --disable-gnome-shell --disable-unity --disable-xfwm
 
 # Installation.
