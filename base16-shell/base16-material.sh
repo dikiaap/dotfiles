215bab105a15104299f14b3456bc6d14075e0883
Diki Ananta <diki1aap@gmail.com>
2017-04-13 18:32:59 +0700

    shell: #!/usr/bin/env

--- base16-shell/base16-material.sh
+++ base16-shell/base16-material.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env sh
 # base16-shell (https://github.com/chriskempson/base16-shell)
 # Base16 Shell template by Chris Kempson (http://chriskempson.com)
 # Material scheme by https://github.com/dikiaap
