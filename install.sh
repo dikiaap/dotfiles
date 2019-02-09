f0f6c34d182fa155cf2e451a9ee0060bf3957d24
Diki Ananta <diki1aap@gmail.com>
2019-02-09 09:06:11 +0700

    install: improve script

--- install.sh
+++ install.sh
@@ -3,123 +3,133 @@
 blue='\e[1;34m'
 red='\e[1;31m'
 white='\e[0;37m'
+dotfiles_repo_dir=$(pwd)
+backup_dir="$HOME/.dotfiles.orig"
+dotfiles_home_dir=(.zsh .aliases .bash_profile .bashrc .dircolors .editorconfig .exports
+                   .functions .gemrc .ripgreprc .tmux.conf .wgetrc .Xresources .zshrc)
+dotfiles_xdg_config_dir=(.alacritty .compton .dunst .htop .i3 .i3blocks .rofi)
 
-CMD="$1"
-dotfilesdir=$(pwd)
-backupdir=~/.dotfiles.orig
-dotfiles=(.zsh .aliases .bash_profile .bash_prompt .bashrc .dircolors .editorconfig .exports .functions .gemrc .ripgreprc .tmux.conf .wgetrc .Xresources .zshrc)
-dotfiles_config=(.alacritty .compton .dunst .htop .i3 .i3blocks .rofi)
+# Print usage message.
+usage() {
+    local program_name
+    program_name=${0##*/}
+    cat <<EOF
+Usage: $program_name [-option]
 
-printusage() {
-    prog=$(basename "$0")
-    echo "Usage: $prog [-option]" >&2
-    echo "" >&2
-    echo "Options:" >&2
-    echo "    --help    Print this message" >&2
-    echo "    -i        Install all config" >&2
-    echo "    -r        Restore old config" >&2
+Options:
+    --help    Print this message
+    -i        Install all config
+    -r        Restore old config
+EOF
 }
 
-install() {
+install_dotfiles() {
     # Backup config.
-    if ! [ -f $backupdir/check-backup.txt ]; then
-        mkdir -p $backupdir/.config
-        cd $backupdir
+    if ! [ -f "$backup_dir/check-backup.txt" ]; then
+        mkdir -p "$backup_dir/.config"
+        cd "$backup_dir" || exit
         touch check-backup.txt
 
         # Backup to ~/.dotfiles.orig
-        for dots in "${dotfiles[@]}"
+        for dots_home in "${dotfiles_home_dir[@]}"
         do
-            /bin/cp -rf ~/${dots} $backupdir &> /dev/null
+            env cp -rf "$HOME/${dots_home}" "$backup_dir" &> /dev/null
         done
 
         # Backup some folder in ~/.config to ~/.dotfiles.orig/.config
-        for dots_conf in "${dotfiles_config[@]//./}"
+        for dots_xdg_conf in "${dotfiles_xdg_config_dir[@]//./}"
         do
-            /bin/cp -rf ~/.config/${dots_conf} $backupdir/.config &> /dev/null
+            env cp -rf "$HOME/.config/${dots_xdg_conf}" "$backup_dir/.config" &> /dev/null
         done
 
         # Backup again with Git.
-        git init &> /dev/null
-        git add -u &> /dev/null
-        git add . &> /dev/null
-        git commit -m "Backup original config on `date '+%Y-%m-%d %H:%M'`" &> /dev/null
+        if [ -x "$(command -v git)" ]; then
+            git init &> /dev/null
+            git add -u &> /dev/null
+            git add . &> /dev/null
+            git commit -m "Backup original config on $(date '+%Y-%m-%d %H:%M')" &> /dev/null
+        fi
 
         # Output.
-        echo -e $blue"Your config is backed up in "$backupdir"\n" >&2
-        echo -e $red"Please do not delete check-backup.txt in .dotfiles.orig folder."$white >&2
+        echo -e "${blue}Your config is backed up in ${backup_dir}\n" >&2
+        echo -e "${red}Please do not delete check-backup.txt in .dotfiles.orig folder.${white}" >&2
         echo -e "It's used to backup and restore your old config.\n" >&2
     fi
 
     # Install config.
-    for dots in "${dotfiles[@]}"
+    for dots_home in "${dotfiles_home_dir[@]}"
     do
-        /bin/rm -rf ~/${dots}
-        /bin/ln -fs "$dotfilesdir/${dots}" ~/
+        env rm -rf "$HOME/${dots_home}"
+        env ln -fs "$dotfiles_repo_dir/${dots_home}" "$HOME/"
     done
 
     # Install config to ~/.config.
-    mkdir -p ~/.config
-    for dots_conf in "${dotfiles_config[@]}"
+    mkdir -p "$HOME/.config"
+    for dots_xdg_conf in "${dotfiles_xdg_config_dir[@]}"
     do
-        /bin/rm -rf ~/.config/${dots_conf[@]//./}
-        /bin/ln -fs "$dotfilesdir/${dots_conf}" ~/.config/${dots_conf[@]//./}
+        env rm -rf "$HOME/.config/${dots_xdg_conf[*]//./}"
+        env ln -fs "$dotfiles_repo_dir/${dots_xdg_conf}" "$HOME/.config/${dots_xdg_conf[*]//./}"
     done
 
-    echo -e $blue"New dotfiles is installed!\n"$white >&2
+    echo -e "${blue}New dotfiles is installed!\n${white}" >&2
     echo "There may be some errors when Terminal is restarted." >&2
     echo "Please read carefully the error messages and make sure all packages are installed. See more info in README.md." >&2
     echo "Note that the author of this dotfiles uses dev branch in some packages." >&2
-    echo -e "If you want to restore your old config, you can use "$red"./install.sh -r"$white" command." >&2
+    echo -e "If you want to restore your old config, you can use ${red}./install.sh -r${white} command." >&2
 }
 
-uninstall() {
-    if [ -f $backupdir/check-backup.txt ]; then
-        for dots in "${dotfiles[@]}"
+uninstall_dotfiles() {
+    if [ -f "$backup_dir/check-backup.txt" ]; then
+        for dots_home in "${dotfiles_home_dir[@]}"
         do
-            /bin/rm -rf ~/${dots}
-            /bin/cp -rf $backupdir/${dots} ~/ &> /dev/null
-            /bin/rm -rf $backupdir/${dots}
+            env rm -rf "$HOME/${dots_home}"
+            env cp -rf "$backup_dir/${dots_home}" "$HOME/" &> /dev/null
+            env rm -rf "$backup_dir/${dots_home}"
         done
 
-        for dots_conf in "${dotfiles_config[@]//./}"
+        for dots_xdg_conf in "${dotfiles_xdg_config_dir[@]//./}"
         do
-            /bin/rm -rf ~/.config/$dots_conf
-            /bin/cp -rf $backupdir/.config/${dots_conf} ~/.config &> /dev/null
-            /bin/rm -rf $backupdir/.config/${dots_conf}
+            env rm -rf "$HOME/.config/${dots_xdg_conf}"
+            env cp -rf "$backup_dir/.config/${dots_xdg_conf}" "$HOME/.config" &> /dev/null
+            env rm -rf "$backup_dir/.config/${dots_xdg_conf}"
         done
 
         # Save old config in backup directory with Git.
-        cd $backupdir &> /dev/null
-        git add -u &> /dev/null
-        git add . &> /dev/null
-        git commit -m "Restore original config on `date '+%Y-%m-%d %H:%M'`" &> /dev/null
+        if [ -x "$(command -v git)" ]; then
+            cd "$backup_dir" || exit
+            git add -u &> /dev/null
+            git add . &> /dev/null
+            git commit -m "Restore original config on $(date '+%Y-%m-%d %H:%M')" &> /dev/null
+        fi
     fi
 
-    if ! [ -f $backupdir/check-backup.txt ]; then
-        echo -e $red"You have not installed this dotfiles yet."$white >&2
+    if ! [ -f "$backup_dir/check-backup.txt" ]; then
+        echo -e "${red}You have not installed this dotfiles yet.${white}" >&2
     else
-        echo -e $blue"Your old config has been restored!\n"$white >&2
+        echo -e "${blue}Your old config has been restored!\n${white}" >&2
         echo "Thanks for using my dotfiles." >&2
         echo "Enjoy your next journey!" >&2
     fi
 
-    /bin/rm -rf $backupdir/check-backup.txt
+    env rm -rf "$backup_dir/check-backup.txt"
 }
 
-if [[ -z "$1" ]] || [[ "$1" == "-h" ]] || [[ "$1" == "--help" ]]; then
-    printusage
-    exit 0
-fi
+main() {
+    case "$1" in
+        ''|-h|--help)
+            usage
+            exit 0
+            ;;
+        -i)
+            install_dotfiles
+            ;;
+        -r)
+            uninstall_dotfiles
+            ;;
+        *)
+            echo "Command not found" >&2
+            exit 1
+    esac
+}
 
-case "$CMD" in
-    -i)
-        install
-        ;;
-    -r)
-        uninstall
-        ;;
-    *)
-        echo "Command not found" >&2
-        exit 1
-esac
+main "$@"
